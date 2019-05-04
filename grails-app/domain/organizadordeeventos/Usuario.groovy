866c0e8beb6cdfc844639078123dd71046b0e2fa
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2019-05-04 17:00:45 -0300

    prueba

--- grails-app/domain/organizadordeeventos/Usuario.groovy
+++ grails-app/domain/organizadordeeventos/Usuario.groovy
@@ -3,12 +3,14 @@ package organizadordeeventos
 class Usuario {
 
     String nombre
+    String apellido
     String mail
     Evento evento
 
     static constraints = {
 
         nombre blank: false, nullable: false
+        apellido blank: false, nullable: false
         mail blank: false, nullable: false, mail: true
 
     }
