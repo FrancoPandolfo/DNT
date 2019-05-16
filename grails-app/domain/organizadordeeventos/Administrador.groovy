e3c03d7149cd43bd12e1aee9bc85c43e61b79621
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2019-05-16 15:14:09 -0300

    sigo agregando cosas, hay varias cosas que revisar. Administrador como un estado y como dividir las tareas que hay en Evento

---
+++ grails-app/domain/organizadordeeventos/Administrador.groovy
@@ -0,0 +1,22 @@
+package organizadordeeventos
+
+class Administrador {
+    //el administrador probablemente herede de usuario
+    //la diferencia es que va a poder dar uso de otros metodos
+    //es un estado pero restringido a un evento. como va cambiando de estado?
+
+    //agregar invitado
+
+    //agregar proveedor
+
+    //agregar infraestructura
+
+    //agregar presupuesto
+
+    //agregar transporte
+
+    //podria haber una clase personal(limpieza,seguridad,etc...)?
+
+    static constraints = {
+    }
+}
