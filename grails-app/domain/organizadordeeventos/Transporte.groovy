0f4334ebc9501889e22566841df483d3032f3ab0
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2019-05-16 15:49:09 -0300

    sigo agregando cosas, hay varias cosas que revisar. Administrador como un estado y como dividir las tareas que hay en Evento

---
+++ grails-app/domain/organizadordeeventos/Transporte.groovy
@@ -0,0 +1,10 @@
+package organizadordeeventos
+
+class Transporte {
+
+    String nombre
+    String descripcion
+
+
+    //falta hacer el constructor?
+}
---
+++ src/test/groovy/organizadordeeventos/TransporteSpec.groovy
@@ -0,0 +1,18 @@
+package organizadordeeventos
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class TransporteSpec extends Specification implements DomainUnitTest<Transporte> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
