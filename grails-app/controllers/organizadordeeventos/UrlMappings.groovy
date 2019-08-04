768d5a7cce280e5416757234ba05a56c658024aa
augusjoa <augusjoaliza@hotmail.com>
2019-08-04 11:44:27 -0300

    Varias vistas

--- grails-app/controllers/organizadordeeventos/UrlMappings.groovy
+++ grails-app/controllers/organizadordeeventos/UrlMappings.groovy
@@ -4,15 +4,15 @@ class UrlMappings {
 
     static mappings = {
 
-        "/mostrarUsuarios"(controller: 'prueba4Controller')
+        //"/mostrarUsuarios"(controller: 'prueba4Controller')
 
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
-
-        "/"(view:"/index")
+        "/"(controller: "home")
+        //"/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
